b6e392703c6700eaf03cee8b99c097b0141723b1
aherlihy <herlihyap@gmail.com>
2022-12-15 19:44:21 +0100

    remove debug

--- src/main/scala/datalog/tools/Debug.scala
+++ src/main/scala/datalog/tools/Debug.scala
@@ -5,5 +5,5 @@ import datalog.storage.StorageManager
 import scala.collection.immutable
 
 object Debug:
-  inline val on = true
+  inline val on = false
   inline def debug(prefix: String, f: () => String): Unit = if (on) println(prefix + f())
