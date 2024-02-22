9c71833666b82611245e2607bf294d58ed90c498
aherlihy <herlihyap@gmail.com>
2024-02-22 14:23:00 +0100

    with printlines

--- src/main/scala/datalog/execution/ir/IROp.scala
+++ src/main/scala/datalog/execution/ir/IROp.scala
@@ -108,10 +108,10 @@ case class DoWhileOp(toCmp: DB, override val children:IROp[Any]*)(using JITOptio
   override def run(storageManager: StorageManager): Any =
     var i = 0
     while ( {
-//      children.head.children.head.run(storageManager) // swap
-//      println(s"DBs start of semi-naive iteration $i: ${storageManager.toString}")
-//      children.head.children(1).run(storageManager)
-      children.head.run(storageManager)
+      children.head.children.head.run(storageManager) // swap
+      println(s"DBs start of semi-naive iteration $i: ${storageManager.toString}")
+      children.head.children(1).run(storageManager)
+//      children.head.run(storageManager)
       i += 1
 //      if i > 3 then System.exit(0)
       children.head.run(storageManager)
@@ -234,7 +234,7 @@ case class ProjectJoinFilterOp(rId: RelationId, var k: JoinIndexes, override val
       jitOptions.onlineSort
     )
   override def run(storageManager: StorageManager): EDB =
-//    println(s"doing SPJU on children: ${children.map(c => c.asInstanceOf[ScanOp]).map(c => s"${storageManager.ns(c.rId)}.${c.db}.${c.knowledge}")}")
+    println(s"doing SPJU on children: ${children.map(c => c.asInstanceOf[ScanOp]).map(c => s"${storageManager.ns(c.rId)}.${c.db}.${c.knowledge}")}")
     val inputs = children.map(s => s.run(storageManager))
     val res = storageManager.joinProjectHelper_withHash(
         inputs,
@@ -242,7 +242,7 @@ case class ProjectJoinFilterOp(rId: RelationId, var k: JoinIndexes, override val
         k.hash,
         jitOptions.onlineSort
       )
-//    println(s"=> result of SPJU on ${storageManager.printer.ruleToString(k.atoms)}: ${storageManager.ns(rId)}=${res.factToString}")
+    println(s"=> result of SPJU on ${storageManager.printer.ruleToString(k.atoms)}: ${storageManager.ns(rId)}=${res.factToString}")
     res
 }
 
