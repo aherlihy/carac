9c72611e0bf1133c6a8aacd5ba90a42cbbb17f56
aherlihy <herlihyap@gmail.com>
2024-02-28 21:12:24 +0100

    add csda test

---
+++ src/test/scala/test/examples/csda_full/csda.scala
@@ -0,0 +1,29 @@
+package test.examples.csda_full
+
+import buildinfo.BuildInfo
+import datalog.dsl.{Constant, Program}
+import test.{ExampleTestGenerator, Tags}
+
+class csda_test extends ExampleTestGenerator(
+  "csda_full",
+  Set(Tags.Naive, Tags.Volcano),
+  Set(Tags.Slow, Tags.CI)
+) with csda
+
+trait csda {
+  val factDirectory = s"${BuildInfo.baseDirectory}/src/test/scala/test/examples/csda_full/facts"
+  val toSolve = ""
+
+  // Adapted from https://drive.google.com/drive/mobile/folders/1M4WxwykUd-jX8jBA50pSNf2R-1IJ49PJ/1DRj-cfISV9v34vU7DH1PKEu13PaMIf71/1V1JWRdDzjjGxBPSKLg6diEAjJ-eRbQvj?usp=drive_link&pli=1&sort=13&direction=a
+  def pretest(program: Program): Unit = {
+    val NullEdge = program.namedRelation("NullEdge")
+    val Arc = program.namedRelation("Arc")
+
+    val Null = program.relation[Int]("Null")
+
+    val x, y, w = program.variable()
+
+    Null(x, y) :- NullEdge(x, y)
+    Null(x, y) :- (Null(x, w), Arc(w, y))
+  }
+}
