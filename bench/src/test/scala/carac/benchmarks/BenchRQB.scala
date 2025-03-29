aa5941bf45dca3929aebaa7ad1519f24c20993e6
aherlihy <herlihyap@gmail.com>
2025-03-29 16:15:39 +0100

    Andersen updates

--- bench/src/test/scala/carac/benchmarks/BenchRQB.scala
+++ bench/src/test/scala/carac/benchmarks/BenchRQB.scala
@@ -15,7 +15,7 @@ import scala.quoted.staging
 import scala.sys.process.Process
 import scala.util.Using
 
-val SOUFFLE_BIN="souffle"
+val SOUFFLE_BIN="/scratch/herlihy/souffle/build/src/souffle"
 
 @Fork(1) // # of jvms that it will use
 @Warmup(iterations = 10, time = 10, timeUnit = TimeUnit.SECONDS, batchSize = 1)
@@ -39,19 +39,19 @@ class BenchRQB_andersen extends rqb_andersen {
   @Setup(Level.Iteration)
   def s(): Unit = {
     Process(s"rm -rf souffle-out/compile/$benchmark").!
-    Process(s"mkdir souffle-out/compile/$benchmark").!
+    Process(s"mkdir -p souffle-out/compile/$benchmark").!
     Process(s"rm -rf souffle-out/interp/$benchmark").!
-    Process(s"mkdir souffle-out/interp/$benchmark").!
+    Process(s"mkdir -p souffle-out/interp/$benchmark").!
     Process(s"rm -rf souffle-out/profile-compile/$benchmark").!
-    Process(s"mkdir souffle-out/profile-compile/$benchmark").!
+    Process(s"mkdir -p souffle-out/profile-compile/$benchmark").!
     Process(s"rm -r $benchmark-profile-compile")
     Process(s"rm -rf souffle-out/profile-interp/$benchmark").!
-    Process(s"mkdir souffle-out/profile-interp/$benchmark").!
+    Process(s"mkdir -p souffle-out/profile-interp/$benchmark").!
     Process(s"rm -r $benchmark-profile-interp")
     Process(s"rm -rf carac-out/$benchmark").!
-    Process(s"mkdir carac-out/$benchmark").!
+    Process(s"mkdir -p carac-out/$benchmark").!
     Process(s"rm -rf carac-scala-out/$benchmark").!
-    Process(s"mkdir carac-scala-out/$benchmark").!
+    Process(s"mkdir -p  carac-scala-out/$benchmark").!
   }
 
   @Benchmark def carac_warm_bytecode(blackhole: Blackhole): Unit = {
@@ -86,13 +86,29 @@ class BenchRQB_andersen extends rqb_andersen {
       })
   }
 
-  @Benchmark def carac_native_lambda(blackhole: Blackhole): Unit = {
-    val b = "lambda"
-    val pb = Process(Seq(s"../target/native-image/carac", benchmark, b))
-    val exitCode = pb.!
-    if (exitCode != 0) throw new Exception(s"Carac $benchmark and $b exited with code $exitCode")
-    blackhole.consume(exitCode) // prob unnecessary
+  @Benchmark def carac_warm_interp(blackhole: Blackhole): Unit = {
+    val jo = JITOptions(mode = CaracMode.Interpreted, sortOrder = SortOrder.Sel)
+    val engine = new StagedExecutionEngine(new DuckDBStorageManager(), jo)
+    val program = Program(engine)
+    program.loadFromFactDir(factDirectory)
+    pretest(program)
+    blackhole.consume(
+      program.namedRelation(toSolve).solve()
+    )
+    engine.precedenceGraph.idbs.foreach(i =>
+      val idb = engine.storageManager.ns(i)
+      Using(Files.newBufferedWriter(Paths.get("carac-scala-out", benchmark, idb + ".csv"))) { writer =>
+        engine.get(idb).foreach(f => writer.write(f.mkString("", "\t", "\n")))
+      })
   }
+
+//  @Benchmark def carac_native_lambda(blackhole: Blackhole): Unit = {
+//    val b = "lambda"
+//    val pb = Process(Seq(s"../target/native-image/carac", benchmark, b))
+//    val exitCode = pb.!
+//    if (exitCode != 0) throw new Exception(s"Carac $benchmark and $b exited with code $exitCode")
+//    blackhole.consume(exitCode) // prob unnecessary
+//  }
 
   @Benchmark def carac_jar_lambda(blackhole: Blackhole): Unit = {
     val b = "lambda"
@@ -110,8 +126,15 @@ class BenchRQB_andersen extends rqb_andersen {
     blackhole.consume(exitCode) // prob unnecessary
   }
 
+  @Benchmark def carac_jar_interp(blackhole: Blackhole): Unit = {
+    val b = "Interpreted"
+    val pb = Process(Seq(s"../target/pack/bin/main", benchmark, b))
+    val exitCode = pb.!
+    if (exitCode != 0) throw new Exception(s"Carac $benchmark and $b exited with code $exitCode")
+    blackhole.consume(exitCode) // prob unnecessary
+  }
+
   private def run_souffle(mode: String, blackhole: Blackhole): Unit = {
-    println(s"running src/test/scala/carac/benchmarks/souffle/souffle-driver.sh $benchmark $mode")
     val pb = Process(Seq("src/test/scala/carac/benchmarks/souffle/souffle-driver.sh", SOUFFLE_BIN, benchmark, mode))
     val exitCode = pb.!
     if (exitCode != 0) throw new Exception(s"Souffle $mode failed with code $exitCode")
